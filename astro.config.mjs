bbc7b566373918949a15ca6f66d1757441d52f44
Sebastian Bravo <sebastianalejandrobravocampos@gmail.com>
2025-06-08 01:55:08 -0400

    chore(config): actualizar configuración del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vendor': ['astro'],
+          }
+        }
+      }
+    },
   },
-  output: 'static',
+  output: 'server',
+  compressHTML: true,
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    imageService: true,
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      domains: [],
+    },
+  }),
 });
